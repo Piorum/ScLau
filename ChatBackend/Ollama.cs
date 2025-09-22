ce46c6969094191c7e75e5b077f1cab0ecb5f1e8
Piorum <90207649+Piorum@users.noreply.github.com>
2025-09-22 07:50:22 -0600

    Model Params Now Defined By Environment

--- ChatBackend/Ollama.cs
+++ ChatBackend/Ollama.cs
@@ -10,31 +10,29 @@ public class OllamaRequest
     [JsonPropertyName("prompt")]
     public string? Prompt { get; set; }
     [JsonPropertyName("model")]
-    public string? Model { get; set; }
+    public string? Model { get; set; } = Environment.GetEnvironmentVariable("MODEL");
     [JsonPropertyName("options")]
-    public OllamaOptions? Options { get; set; }
+    public OllamaOptions Options { get; set; } = new();
     [JsonPropertyName("raw")]
     public bool Raw { get; set; } = true;
-    [JsonPropertyName("stop")]
-    public string[]? StopTokens { get; set; } = ["<|return|>", "<|call|>"];
 }
 
 public class OllamaOptions
 {
     [JsonPropertyName("temperature")]
-    public double Temperature { get; set; } = 1.0;
+    public double Temperature { get; set; } = double.TryParse(Environment.GetEnvironmentVariable("TEMPERATURE"), out var _temperature) ? _temperature : 1.0;
     [JsonPropertyName("num_predict")]
-    public int NumPredict { get; set; } = -2; //Fill context (-1 == Inifinte)
+    public int NumPredict { get; set; } = int.TryParse(Environment.GetEnvironmentVariable("NUM_PREDICT"), out var _numPredict) ? _numPredict : -2; //Fill context (-1 == Inifinte)
     [JsonPropertyName("num_ctx")]
-    public int NumCtx { get; set; } = 8192;
+    public int NumCtx { get; set; } = int.TryParse(Environment.GetEnvironmentVariable("NUM_CTX"), out var _numCtx) ? _numCtx : 8192;
     [JsonPropertyName("repeat_penalty")]
-    public double RepeatPenalty { get; set; } = 1.0;
+    public double RepeatPenalty { get; set; } = double.TryParse(Environment.GetEnvironmentVariable("REPEAT_PENALTY"), out var _repPenalty) ? _repPenalty : 1.2;
     [JsonPropertyName("top_k")]
-    public int TopK { get; set; } = 40;
+    public int TopK { get; set; } = int.TryParse(Environment.GetEnvironmentVariable("TOP_K"), out var _topK) ? _topK : 40;
     [JsonPropertyName("top_p")]
-    public double TopP { get; set; } = 0.95;
+    public double TopP { get; set; } = double.TryParse(Environment.GetEnvironmentVariable("TOP_P"), out var _topP) ? _topP : 0.95;
     [JsonPropertyName("min_p")]
-    public double MinP { get; set; } = 0.05;
+    public double MinP { get; set; } = double.TryParse(Environment.GetEnvironmentVariable("MIN_P"), out var _minP) ? _minP : 0.05;
 
 }
 
@@ -57,18 +55,9 @@ public static class Ollama
             string url = Environment.GetEnvironmentVariable("OLLAMA_ENDPOINT") ?? throw new("OLLAMA_ENDPOINT was null");
             var requestBody = new OllamaRequest
             {
-                Prompt = "<|start|>system<|message|>You are a large language model (LLM).\nKnowledge cutoff: 2024-06\nCurrent date: 2025-06-28\n\nReasoning: high\n\n# Valid channels: analysis, commentary, final. Channel must be included for every message.\nCalls to these tools must go to the commentary channel: 'functions'.<|end|><|start|>developer<|message|># Instructions\n\nFulfill the request to the best of your abilities\n\n# Tools\n\n## functions\n\nnamespace functions {\n\n// Gets the location of the user.\ntype get_location = () => any;\n\n// Gets the current weather in the provided location.\ntype get_current_weather = (_: {\n// The city and state, e.g. San Francisco, CA\nlocation: string,\nformat?: \"celsius\" | \"fahrenheit\", // default: celsius\n}) => any;\n\n// Gets the current weather in the provided list of locations.\ntype get_multiple_weathers = (_: {\n// List of city and state, e.g. [\"San Francisco, CA\", \"New York, NY\"]\nlocations: string[],\nformat?: \"celsius\" | \"fahrenheit\", // default: celsius\n}) => any;\n\n} // namespace functions<|end|><|start|>user<|message|>" + prompt + "<|end|><|start|>assistant",
-                //Base Model: (https://huggingface.co/huihui-ai/Huihui-gpt-oss-20b-BF16-abliterated) | Stop tokens adjusted via Modelfile (["<|return|>", "<|call|>"])
-                Model = "gpt-oss-20b-abliterated:latest",
-                Options = new OllamaOptions
-                {
-                    Temperature = 0.6,
-                    NumPredict = 2000,
-                    NumCtx = 8192,
-                    RepeatPenalty = 1.15
-                },
-                Raw = true,
-                StopTokens = null
+                Prompt = "<|start|>system<|message|>You are a large language model (LLM).\nKnowledge cutoff: 2024-06\nCurrent date: 2025-06-28\n\nReasoning: medium\n\n# Valid channels: analysis, commentary, final. Channel must be included for every message.\nCalls to these tools must go to the commentary channel: 'functions'.<|end|><|start|>developer<|message|># Instructions\n\nFulfill the request to the best of your abilities\n\n<|end|><|start|>user<|message|>" + prompt + "<|end|><|start|>assistant",
+                //Ex tool string: (Inject directly after instructions in developer channel) "# Tools\n\n## functions\n\nnamespace functions {\n\n// Gets the location of the user.\ntype get_location = () => any;\n\n// Gets the current weather in the provided location.\ntype get_current_weather = (_: {\n// The city and state, e.g. San Francisco, CA\nlocation: string,\nformat?: \"celsius\" | \"fahrenheit\", // default: celsius\n}) => any;\n\n// Gets the current weather in the provided list of locations.\ntype get_multiple_weathers = (_: {\n// List of city and state, e.g. [\"San Francisco, CA\", \"New York, NY\"]\nlocations: string[],\nformat?: \"celsius\" | \"fahrenheit\", // default: celsius\n}) => any;\n\n} // namespace functions"
+                Raw = true
             };
             var jsonRequest = JsonSerializer.Serialize(requestBody);
             var request = new HttpRequestMessage(HttpMethod.Post, url)
