4a27913de6259f27b12be77d104b1f7acbc9e3b0
Piorum <90207649+Piorum@users.noreply.github.com>
2025-09-24 10:43:26 -0600

    Fixed Loading Icon Visual Bug

--- chatfrontend/src/components/LoadingIcon.tsx
+++ chatfrontend/src/components/LoadingIcon.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './LoadingIcon.css';
 
 const LoadingIcon: React.FC = () => {
-  return <div className="loading-icon"></div>;
+  return (
+    <div className="loading-icon-wrapper">
+      <div className="loading-icon"></div>
+    </div>
+  );
 };
 
 export default LoadingIcon;
