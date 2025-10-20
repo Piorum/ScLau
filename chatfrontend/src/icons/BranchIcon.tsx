4a3fd79849c30dc11fca3ca5a1f1fc9eb42b586f
Piorum <90207649+Piorum@users.noreply.github.com>
2025-10-20 13:25:40 -0600

    Minor BranchIcon

--- chatfrontend/src/icons/BranchIcon.tsx
+++ chatfrontend/src/icons/BranchIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BranchIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M 6 9 A 1 1 0 0 0 6 3 A 1 1 0 0 0 6 9 L 6 17 A 1 1 0 0 0 6 23 A 1 1 0 0 0 6 17 c 0 -8 12 -4 12 -8 A 1 1 0 0 0 18 3 A 1 1 0 0 0 18 9 C 18 13 6 9 6 17 L 6 9 Z" />
+    <path d="M 6 9 A 1 1 0 0 0 6 3 A 1 1 0 0 0 6 9 L 6 17 A 1 1 0 0 0 6 23 A 1 1 0 0 0 6 17 C 6 9 18 17 18 9 A 1 1 0 0 0 18 3 A 1 1 0 0 0 18 9 c 0 8 -12 0 -12 8 L 6 9 Z" />
   </svg>
 );
 
